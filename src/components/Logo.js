c734b90bbe3f2dbe9c017cc504417b49fbb92241
SangYong-Jeong <mmaf@naver.com>
2021-11-24 15:34:23 +0900

    first commit - logo

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <h1 className="text-center fa-2x py-4 text-primary">
+      <i className="fa fa-search"></i> JSY FIND
+    </h1>
+  );
+};
+
+export default Logo;
